660dc2a486c0f0c32afe125104f7b2a1805aa66a
Toporasca Bardita <romaniaromania90@yahoo.com>
2017-06-19 17:17:43 +0300

    Commited

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+$("button").on("click", function() {
+
+
+
 $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+	var api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d27525a5c89546287b4b541007f4042b";
+
 	console.log(data);
-	var object = JSON.stringify(data);
-	var location = JSON.parse(object);
-	console.log(location);
+	var location = JSON.stringify(data);
+	var div = document.getElementById("location");
+	/*var city = data.city;*/
+	div.innerHTML = data.city + data.country;
+	/*console.log(location);
+	console.log(data.country);*/
+	$.getJSON(api, function(weather) {
+	console.log(weather.coord.lat);
+});
+	
 });
 
+/*$.getJSON(api, function(weather) {
+	console.log(weather.coord.lat);
+});*/
+	
+
+});
