68bc084b731d70abd9b6a8895242cbc5ea1d1466
Toporasca Bardita <romaniaromania90@yahoo.com>
2017-06-19 17:56:53 +0300

    Commited

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
+$(document).ready(function () {
+
+
 $("button").on("click", function() {
 
+// Convert fahrenehit to celcius
 
-
-$.getJSON("http://ip-api.com/json/?callback=?", function(data) {
-	var api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d27525a5c89546287b4b541007f4042b";
+$.getJSON("http://crossorigin.me/http://ip-api.com/json/?callback=?", function(data) {
+	var zip = data.zip;
+	var code = data.countryCode;
+	var api = "http://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + code + "&appid=d27525a5c89546287b4b541007f4042b";
 
 	console.log(data);
 	var location = JSON.stringify(data);
 	var div = document.getElementById("location");
 	/*var city = data.city;*/
+	var zip = data.zip;
+	var code = data.countryCode;
 	div.innerHTML = data.city + data.country;
 	/*console.log(location);
 	console.log(data.country);*/
 	$.getJSON(api, function(weather) {
-	console.log(weather.coord.lat);
+	console.log(weather);
 });
 	
 });
@@ -22,5 +29,7 @@ $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
 	console.log(weather.coord.lat);
 });*/
 	
+
+});
 
 });
