f797bd8c8b68ec2cfc611855a2459f7d532b491a
Toporasca Bardita <romaniaromania90@yahoo.com>
2017-06-19 18:06:22 +0300

    Commited

--- main.js
+++ main.js
@@ -5,17 +5,18 @@ $("button").on("click", function() {
 
 // Convert fahrenehit to celcius
 
-$.getJSON("http://crossorigin.me/http://ip-api.com/json/?callback=?", function(data) {
-	var zip = data.zip;
-	var code = data.countryCode;
-	var api = "http://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + code + "&appid=d27525a5c89546287b4b541007f4042b";
+$.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+	/*var zip = data.zip;
+	var code = data.countryCode;*/
+	var lat = data.lat;
+	var lon = data.lon;
+	/*var api = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + code + "&appid=d27525a5c89546287b4b541007f4042b";*/
+	var api = "http://api.apixu.com/v1/current.json?key=eac7ca1087144cc6b1c130638171606&q=" + lat + "," + lon;
 
 	console.log(data);
 	var location = JSON.stringify(data);
 	var div = document.getElementById("location");
 	/*var city = data.city;*/
-	var zip = data.zip;
-	var code = data.countryCode;
 	div.innerHTML = data.city + data.country;
 	/*console.log(location);
 	console.log(data.country);*/
